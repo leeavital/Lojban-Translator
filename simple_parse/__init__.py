ab710f987e9465b510c602132b896a6005add376
Lee Avital <lee@lee-Ideapad-Z575.(none)>
2013-01-09 12:53:30 -0500

    Added files

---
+++ simple_parse/__init__.py
@@ -0,0 +1,7 @@
+'''Simple parsing using mxTextTools
+
+See the /doc subdirectory for introductory and
+general documentation.  See license.txt for licensing
+information.  (This is a BSD-licensed package).
+'''
+__version__="2.1.1a2"
