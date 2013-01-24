eb7b8378aaa0b6491cfe55961f791d94082bd323
Lee Avital <leeavital@gmail.com>
2013-01-23 21:00:09 -0500

    Got rid of simpleparse, added examples for pyparse

--- simple_parse/build/lib.linux-x86_64-2.7/simpleparse/__init__.py
+++
@@ -1,7 +0,0 @@
-'''Simple parsing using mxTextTools
-
-See the /doc subdirectory for introductory and
-general documentation.  See license.txt for licensing
-information.  (This is a BSD-licensed package).
-'''
-__version__="2.1.1a2"
