5a1b501102619d143c8e5ece4a2b8298880470b7
Lee Avital <leeavital@gmail.com>
2013-01-28 19:31:32 -0500

    Added simple ebnf on ebnf branch

--- simple_rules.py
+++ simple_rules.py
@@ -1,51 +1,19 @@
 from pyparsing import *
 
 
-# here are the rules
-
-# forwand declarations
-lojban_sentence = Forward()
-sumti = Forward()
+sentence =  Forward()
+terms = Forward()
+bridi_tail = Forward()
+bridi_tail_1 = Forward() 
+bridi_tail_2 = Forward()
+bridi_tail_3 = Forward()
 selbri = Forward()
-bridi_3 = Forward()
+tail_terms = Forward()
 
 
-lojban_sentence <<  ( LineStart() + bridi_3 + LineEnd() )
+sentence << ZeroOrMore(terms) + bridi_tail
+bridi_tail << bridi_tail_1
+bridi_tail_1 << bridi_tail_2
+bridi_tail_2 << bridi_tail_3
+bridi_tail_3 << selbri + tail_terms
 
-
-
-# literal rules for gismu (the simplest words with meaning)
-gismu = Literal("mi") ^ Literal("ta") ^ Literal("ti")
-
-
-# a sumti can be a gismu (or other things, but only gismu for now)
-sumti << gismu ^ bridi_3
-
-
-selbri <<  Literal("vecnu")
-
-
-# there are three forms of a bridi/3
-bridi_3 <<  ( sumti + selbri + sumti + sumti ) 
-bridi_3 ^=  ( selbri + sumti + sumti + sumti )
-bridi_3 ^=  ( sumti + sumti + sumti + selbri )
-bridi_3 ^=  ( sumti + sumti + selbri + sumti )
-
-
-
-
-
-
-def parse( s ): 
-   
-   try:
-      return lojban_sentence.parseString( s ) 
-
-   except:
-      print "cannot parse: %s" % e
-
-
-
-
-print parse( "mi vecnu ta ti" )
-print parse("mi vecnu ta mi vecnu ta ti")
