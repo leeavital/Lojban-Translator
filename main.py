b1da9cd2575c65fc7ed5a188f120e25293e0ed8b
Lee Avital <leeavital@gmail.com>
2013-02-03 14:55:21 -0500

    Integrated main.py with with simple_rules.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import re
+from simple_rules import getEnglishTranslation
+ 
 def main():
 	print("Welcome to Ben, Zach and Lee's Lojban Translator\nEnter a word or phrase in lojban to continue.\nEnter \"exit\" to quit.")
-	i=""
-	while(i!="exit"):
-		i=raw_input()
-		if(i=="exit"):
+	i= ""
+	while i != "exit":
+		i = raw_input()
+		if i == "exit":
 			break
-		t=translate(i)
-		print(t)
+		t = getEnglishTranslation(i)
+		print t
 
-    print("terminating...")
 
-def translate(t):
-    return "placeholder-result"
+
 
 main()
