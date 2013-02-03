8e4914d392d6f9262ff6a0f4a09e614c3d6c09aa
Lee Avital <leeavital@gmail.com>
2013-02-03 15:45:09 -0500

    Got rid of parser.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import re
 from simple_rules import getEnglishTranslation
+
+"""
+Here is the main driver
+"""
+
  
 def main():
 	print("Welcome to Ben, Zach and Lee's Lojban Translator\nEnter a word or phrase in lojban to continue.\nEnter \"exit\" to quit.")
