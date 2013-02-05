e5d55e31877665e799e9efd447ff1c87c397f52b
Lee Avital <leeavital@gmail.com>
2013-02-04 22:11:36 -0500

    Added print statements

--- gismu_rules.py
+++ gismu_rules.py
@@ -129,7 +129,7 @@ def getCmavo():
 
 def getSentenceFromGismu(lojbanWord,arguments,dictionary,dictionary2):
 	curObject=dictionary[lojbanWord]
-	print(curObject.xs)
+	#print(curObject.xs)
 
 	retString=""
 
